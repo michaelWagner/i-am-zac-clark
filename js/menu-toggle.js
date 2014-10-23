2d5f41e24ef2de730683f3f822ecb24e8351e447
Michael Wagner <wagnerdmike@gmail.com>
2014-10-23 01:12:53 -0700

    Add touchswipe for mobile, update mobile css and logo.

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -15,3 +15,35 @@ $(window).on('click', function(e) {
     $('.toggle-nav').click();
   }
 });
+
+$(document).ready(function() {
+    $('.hover').bind('touchstart', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+    $('.hover').bind('touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
+
+// $(window).on('touchend', function() {
+//   $('.menu-arrow').css('background', 'url(../imgs/arrow.png) no-repeat center');
+//   $('.menu-arrow').css('background-size', '100%');
+// }).on('touchcancel', function() {
+//   $('.menu-arrow').css('background', 'url(../imgs/arrow.png) no-repeat center');
+//   $('.menu-arrow').css('background-size', '100%');
+
+// });
+
+$(window).touchwipe({
+  wipeRight: function() {
+    // Close
+    $.sidr('close', 'sidr');
+  },
+  wipeLeft: function() {
+    // Open
+    $.sidr('open', 'sidr');
+  },
+  preventDefaultEvents: false
+});
